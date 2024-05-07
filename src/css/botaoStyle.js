a1415ce18f719fc87f5a6a4fba9555458eb409fc
jpmoraes06 <150357838+jpmoraes06@users.noreply.github.com>
2024-05-07 16:12:30 -0300

    face

---
+++ src/css/botaoStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     borderWidth:1,
+     borderColor:'#59606B',
+     paddingVertical: 20,
+     paddingHorizontal: 40,
+     borderRadius: 10,
+     marginVertical: 10,
+     height: 59
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        fontSize: 16,
+        textAlign: 'center',
+
+        
+    }
+  });
+  
